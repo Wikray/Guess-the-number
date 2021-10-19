f4ad2426e70c82ae25fe86bbbeb74120c9686536
Wikray <92749034+Wikray@users.noreply.github.com>
2021-10-19 22:26:51 +0200

    Colorized
    Colorized the messages

--- game/main.py
+++ game/main.py
@@ -1,17 +1,23 @@
-secret_Number = int(input("Type in the number your partner should guess: "))
+CEND      = '\33[0m'
+CVIOLET = '\33[35m'
+CRED    = '\33[31m'
+CGREEN2  = '\33[92m'
+CBLUE   = '\33[34m'
+
+secret_Number = int(input(CVIOLET + "Type in the number your partner should guess: " + CEND))
 guess = -1
 counter = 0
 
 while guess != secret_Number:
-    guess = int(input("Type in your guess: "))
+    guess = int(input(CBLUE + "Type in your guess: " + CBLUE))
 
     if guess < secret_Number:
-        print("Your guess is too low!")
+        print("Your guess is too " + CRED +"low!" + CEND)
 
     if guess > secret_Number:
-        print("Your guess is too high!")
+        print("Your guess is too " + CRED + "high!" + CEND)
 
     counter = counter + 1
 
-print("Congratulations, you guessed the number in" , counter, "trys")
+print(CBLUE + "Congratulations, you guessed the number " + CVIOLET + str(secret_Number) + CBLUE + " in " + CBLUE + str(counter) + " try/s")
 
